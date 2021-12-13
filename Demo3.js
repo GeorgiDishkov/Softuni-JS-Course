6c8656f8c305f8e9c1f81e432eecc94b76fe9be7
Georgi Dishkov <georgidishkov2@gmail.com>
2021-12-13 09:40:23 +0200

    init

---
+++ Demo3.js
@@ -0,0 +1,19 @@
+functions
+sdfokasdpfk; ljmasd'\ASdv';kalds 
+[f sdaklf|KsetInterval(() => {
+    
+}, 
+
+f sa
+fk
+KFS
+
+AKSF pksf'[l JFK
+sfj
+'ASJ F
+{"LASJFl
+A"SJFlj3lwe;fja 
+
+jF
+ lfjA
+ S'']);]
