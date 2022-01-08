1741b75ca88c9b4638b4a10cba7147de33638581
Georgi Dishkov <georgidishkov2@gmail.com>
2022-01-08 21:13:35 +0200

    Making Updates

--- form validation/app.js
+++ form validation/app.js
@@ -1,15 +1,21 @@
 const text = document.getElementById("textid");
 const numberId = document.getElementById("numid");
+const selected = document.getElementById("cars");
+const firstCB = document.getElementById("ch1");
+const secondCB = document.getElementById("ch2");
+
 let result = [];
 
 let textCheck = false;
 let numCheck = false;
 let chooseCheck = false;
 let radioCheck = false;
-let boxCheck = false;
-
+let boxCheck1 = false;
+let boxCheck2 = false;
+ 
 function checkInputs() {
   //get values from the input
+  const selectValue = selected.value.trim();
   const textValue = text.value.trim();
   const numberValue = numberId.value.trim();
 
@@ -21,22 +27,53 @@ function checkInputs() {
     textCheck = true;
   } else {
     setErrorFor(text);
+    textCheck = false;
   }
   if (numberValidation(numberValue)) {
     setSuccessFor(numberId);
     result.push(numberValue)
+    numCheck = true;
   } else {
     setErrorFor(numberId);
+    numCheck = false;
   }
-  if(textCheck == true ||numCheck == false ||chooseCheck == false ||radioCheck == false || boxCheck == false){
-    document.getElementById("resid").innerHTML=(result.join(`\n`))
-  };
-
+  if (selectValue != ``) {
+    chooseCheck = true;
+    result.push(selectValue);
+  }
+  if (document.getElementById("r1").checked) {
+    result.push(document.getElementById("r1").value)
+    radioCheck = true;
+  }
+  else if (document.getElementById('r2').checked) {
+    result.push(document.getElementById("r2").value)
+    radioCheck = true;
+  }
+  if(firstCB.checked) {
+    result.push(firstCB.value.trim());
+    boxCheck1 = true;
+  }else{
+    result.push("Unchecked")
+    boxCheck1 = false
+  } if(secondCB.checked) {
+    result.push(secondCB.value.trim());
+    boxCheck2 = true;
+  }else{
+    result.push("Unchecked")
+    boxCheck2 = false
+  }
+  if (textCheck == true && numCheck == true && chooseCheck == true && radioCheck == true && (boxCheck1 == true || boxCheck2 == true)) {
+    document.getElementById("resid").innerHTML = (`Text : ${result[0]} \nNumbers : ${result[1]}\nChoose : ${result[2]}\nRadioButton : ${result[3]}\nCheckbox 1 - ${result[4]}\nCheckbox 2 - ${result[5]} `)
+    result = [];
+  }else {
+    document.getElementById("resid").innerHTML = ("Missing parts")
+  }
 }
 
 
 
 // ------------funcions--------------
+
 function textValidation(text) {
   let flag = true;
   let splitedText = text.split(``);
