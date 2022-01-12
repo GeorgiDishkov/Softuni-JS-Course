2595d9b55ba7cd6bf1a239e993c8c3e98f865461
Georgi Dishkov <georgidishkov2@gmail.com>
2022-01-12 19:14:30 +0200

    Updating

--- JS Advance/06.Sum Table/SumTable.js
+++ JS Advance/06.Sum Table/SumTable.js
@@ -1,3 +1,13 @@
 function sumTable() {
-let tables = document.querySelectorAll(`table tr`)
+    let tables = document.querySelectorAll(`table tr td`)
+    let sum = 0;
+
+    for (let i = 1; i < tables.length-2; i += 2) {
+        console.log(tables[i].textContent);
+        const currentNum = Number(tables[i].textContent);
+        sum += currentNum;
+        console.log(currentNum);
+        console.log(sum);
+    }
+    document.getElementById(`sum`).textContent = sum
 }
--- JS Advance/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS Advance/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,13 @@
 function extract(content) {
+    let text = document.getElementById(content).textContent;
+    let result = '';
+    let regex = /\((.+?)\)/g
+    let match = regex.exec(text);
 
+    while (match != null) {
+        result += match[1];
+        result += (`; `)
+        match = regex.exec(text);
+    }
+    return result;
 }
