3d462a8169384f4c25d0fb8c0fc3ff14d3585cd1
Georgi Dishkov <georgidishkov2@gmail.com>
2022-01-17 02:08:23 +0200

    Uploads

--- JS Advance/04. JS-Advanced-DOM-Introduction-Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ JS Advance/04. JS-Advanced-DOM-Introduction-Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -2,7 +2,29 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const input = document.getElementById('searchField').value;
+      let tables = Array.from(document.getElementsByTagName(`td`));
+      let position = 2;
+      let regex = new RegExp(/([A-Za-z]*)@([a-zA-Z]*.?[A-Za-z]*)/);
+      for (let i = 1; i < tables.length; i+=3) {
+         let  name = tables[i].textContent;
+         let  mail = tables[i+1].textContent;
+         let  course = tables[i+2].textContent;
+         let macher = regex.exec(mail);
+         let elements = document.getElementsByTagName(`tr`);
 
+         elements[position].classList.remove("select");
+
+         if(input.length !== 0){
+         if(name.includes(input)||macher[1].includes(input)) {
+         elements[position].classList.add("select");
+         }else if(macher[2].includes(input)) {
+         elements[position].classList.add("select");
+         }else if(course.includes(input)){
+         elements[position].classList.add("select");
+         }
+      }
+      position++;
+      }
    }
 }
--- JS Advance/04. JS-Advanced-DOM-Introduction-Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ JS Advance/04. JS-Advanced-DOM-Introduction-Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  //TODO
+  let text = document.getElementById(`input`).value;
+  document.getElementById(`input`).id = `output`
+
+  let splitedText = text.split(`.`);
+
+
+  for (let i = 0; i < splitedText.length; i+=3) {
+    let separates = splitedText[i]+splitedText[i+1]+splitedText[i+2] + `DOT`
+    document.getElementById(`output`).innerHTML = separates;
+  }
 }
