54e3b183f39abf2328b97d32fd3c06b75e50918e
Georgi Dishkov <georgidishkov2@gmail.com>
2022-01-17 19:26:39 +0200

    Update

--- JS Advance/04. JS-Advanced-DOM-Introduction-Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ JS Advance/04. JS-Advanced-DOM-Introduction-Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -2,29 +2,17 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      const input = document.getElementById('searchField').value;
-      let tables = Array.from(document.getElementsByTagName(`td`));
-      let position = 2;
-      let regex = new RegExp(/([A-Za-z]*)@([a-zA-Z]*.?[A-Za-z]*)/);
-      for (let i = 1; i < tables.length; i+=3) {
-         let  name = tables[i].textContent;
-         let  mail = tables[i+1].textContent;
-         let  course = tables[i+2].textContent;
-         let macher = regex.exec(mail);
-         let elements = document.getElementsByTagName(`tr`);
+      let input = document.getElementById('searchField').value;
+      let tables = Array.from(document.querySelectorAll(`tbody tr`));
 
-         elements[position].classList.remove("select");
-
-         if(input.length !== 0){
-         if(name.includes(input)||macher[1].includes(input)) {
-         elements[position].classList.add("select");
-         }else if(macher[2].includes(input)) {
-         elements[position].classList.add("select");
-         }else if(course.includes(input)){
-         elements[position].classList.add("select");
+      tables.forEach ((el) => {
+         let newInput = input.toLowerCase();
+         let currentEl = el.textContent.toLowerCase();
+         if(currentEl.includes(newInput)) {
+            el.classList.add('select');
+         }else {
+            el.classList.remove('select');
          }
-      }
-      position++;
-      }
+      })
    }
 }
