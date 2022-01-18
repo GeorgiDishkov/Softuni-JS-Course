5ff2f3e83afd07b6aa2fb163e619c9e97ca9bcfc
Georgi Dishkov <georgidishkov2@gmail.com>
2022-01-19 01:09:51 +0200

    Upload

--- JS Advance/04. JS-Advanced-DOM-Introduction-Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ JS Advance/04. JS-Advanced-DOM-Introduction-Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -2,7 +2,33 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
-      
+      let inputData = document.getElementById(`inputs`).children[1].value;
+      let result = clearInput(inputData); 
+      let resturants = {};
+      let tempRest;
+      for (const line of result) {
+         if(line.includes(`-`)){
+               let tempresult = line.split(` `);
+               tempRest = tempresult[02];
+               resturants.resturant = {resturant = tempresult[0]
+               };
+           }else {
+               let [workers ,salary] = line.split(` `);
+               resturants.tempRest = {workers = salary};
+         }
+      }
    }
+}
+function clearInput(input) {
+
+   let regex = /([A-Za-z]*[- 1-90]*,?)/gm;
+
+   let matches = input.match(regex).map((a) => a.trim());
+   let result = [];
+   for (const line of matches) {
+       if(line && line != `,`){
+           result.push(line);
+       }
+   }
+   return result;
 }
