803410c270da14e216fdfad728ecd7b5556a5875
Georgi Dishkov <georgidishkov2@gmail.com>
2022-01-26 23:43:03 +0200

    Update

--- JS Advance/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ JS Advance/DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+    const text = document.getElementById(`newItemText`).value;
+    const value = document.getElementById(`newItemValue`).value;
+    const content = document.getElementById(`menu`);
+
+    if(text != `` && value != ``) {
+        const options = document.createElement('option');
+        options.value = value;
+        options.textContent = text;
+    
+        content.appendChild(options);
+    
+        document.getElementById(`newItemText`).value = ``;
+        document.getElementById(`newItemValue`).value = ``;
+    }
 }
