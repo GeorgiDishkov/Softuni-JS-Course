27f67373ea9bc382a50640f61d1591fad8960aad
Georgi Dishkov <georgidishkov2@gmail.com>
2022-01-27 01:06:55 +0200

    Upload

--- JS Advance/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ JS Advance/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,35 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+
+    let decode = [];
+    let encode = [];
+    document.getElementById(`main`).addEventListener(`click`, onClick);
+    function onClick(e) {
+        if (e.target.tagName == `BUTTON` && e.target.textContent == `Encode and send it`) {
+            let incoming = document.getElementsByTagName(`textarea`)[0].value;
+            if(incoming != ``){
+                decode = [];
+                console.log(incoming);
+                for (const line of incoming) {
+                    let convert = line.charCodeAt();
+                    convert++;
+                    let result = String.fromCharCode(convert);
+                    decode.push(result);
+                }
+                document.getElementsByTagName(`textarea`)[0].value = ``;
+                document.getElementsByTagName(`textarea`)[1].value = decode.join('');
+            }
+
+        } else if (e.target.tagName == `BUTTON` && e.target.textContent == `Decode and read it` && document.getElementsByTagName(`textarea`)[0].value == ``) {
+            encode = [];
+            for (const line of decode) {
+                let convert = line.charCodeAt();
+                convert--;
+                let result = String.fromCharCode(convert);
+                encode.push(result);
+            }
+            document.getElementsByTagName(`textarea`)[0].value = encode.join('');
+            document.getElementsByTagName(`textarea`)[1].value = ``;
+            decode = [];
+        }
+    }
 }
