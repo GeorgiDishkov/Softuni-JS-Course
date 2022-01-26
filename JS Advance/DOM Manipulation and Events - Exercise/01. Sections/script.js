373ee6344be0307afac987a2f626c0b499861b77
Georgi Dishkov <georgidishkov2@gmail.com>
2022-01-26 20:08:27 +0200

    Uploading

--- JS Advance/DOM Manipulation and Events - Exercise/01. Sections/script.js
+++ JS Advance/DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   const mainDiv = document.getElementById('content');
+   mainDiv.addEventListener(`click`, onClick);
+   for (const line of words) {
+      let divCreate = document.createElement('div');
+      let paragraph = document.createElement('p')
+      paragraph.textContent = line;
+      paragraph.style.display = 'none';
+      divCreate.appendChild(paragraph);
+      mainDiv.appendChild(divCreate);
+   }
+
+   function onClick(e) {
+         e.target.children[0].style.display = '';
+         console.log(e.target.children[0]);
+      }
 }
--- JS Advance/DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
+++ JS Advance/DOM Manipulation and Events - Exercise/02. Time-Converter/solution.js
@@ -1,4 +1,33 @@
 function attachEventsListeners() {
+    let time = {
+        days: 1,
+        hours: 24,
+        minutes: 1440,
+        seconds: 86400,
+    }
+    const content = document.getElementsByTagName(`body`);
+    content[0].addEventListener(`click`, onClick);
 
-    console.log('TODO:...');
-}
+    function onClick(e) {
+        if (e.target.type == 'button') {
+            let currentDiv = e.target.parentNode;
+            let currentTime = Number(currentDiv.querySelector(`input[type="text"]`).value);
+            let currentType = currentDiv.querySelector(`input[type="text"]`).id;
+            const answere = caclucate (currentTime , currentType);
+            document.getElementById(`days`).value = answere.days;
+            document.getElementById(`hours`).value = answere.hours;
+            document.getElementById(`minutes`).value = answere.minutes;
+            document.getElementById(`seconds`).value = answere.seconds;
+        }
+    }
+    function caclucate (value , type){
+        let inDays = value / time[type];
+
+        return {
+            days : inDays,
+            hours : inDays * time.hours,
+            minutes : inDays * time.minutes,
+            seconds : inDays * time.seconds
+        }
+    }
+}   
