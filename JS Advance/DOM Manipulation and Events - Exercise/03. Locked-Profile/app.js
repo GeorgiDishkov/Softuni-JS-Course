fbaeb5fab2f9578cddfe71d964c4f8b07f4a8f9a
Georgi Dishkov <georgidishkov2@gmail.com>
2022-01-26 23:28:07 +0200

    Update

--- JS Advance/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ JS Advance/DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,4 +1,20 @@
 function lockedProfile() {
 
+    document.getElementById(`main`).addEventListener('click', onClick);
 
+    function onClick(e) {
+        if (e.target.tagName == `BUTTON`) {
+            let isActive = e.target.parentElement.querySelector(`input[type="radio"][value = "unlock"]`).checked;
+            if (isActive) {
+                if (e.target.textContent == `Show more`) {
+                    e.target.parentNode.querySelector(`div`).style.display = `block`;
+                    e.target.textContent = `Hide it`;
+                } else if (e.target.textContent == `Hide it`) {
+                    e.target.parentNode.querySelector(`div`).style.display = `none`;
+                    e.target.parentNode.querySelector(`div`);
+                    e.target.textContent = `Show more`;
+                }
+            }
+        }
+    }
 }
