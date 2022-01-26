d3118ad943cfac9960bf6a7450975b61c83b9c38
Georgi Dishkov <georgidishkov2@gmail.com>
2022-01-26 19:00:06 +0200

    Upload

---
+++ JS Advance/04. JS-Advanced-DOM-Introduction-Exercise/demo.js
@@ -0,0 +1,8 @@
+function bestRsturant (firstRest , secondRest ) {
+    let firstSum = 0;
+    let secondSum = 0;
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+}
