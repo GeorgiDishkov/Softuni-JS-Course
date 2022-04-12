93bbd267c378b744c7ef7504051dc158c9b178f8
Georgi <60046070+GeorgiDishkov@users.noreply.github.com>
2022-04-12 21:54:38 +0300

    Uploading Homework

--- JS App/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/05. Accordion/accordion.js
+++ JS App/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/05. Accordion/accordion.js
@@ -1,3 +1,54 @@
 function solution() {
-    //TODO .....
+
+
+    let main = document.querySelector(`#main`);
+    let lsitUrl = `http://localhost:3030/jsonstore/advanced/articles/list`
+    fetch(lsitUrl)
+        .then(response => {
+            response
+                .json()
+            .then(data => {
+                for (const line of data) {
+                    let acordeon = document.createElement(`div`);
+                    acordeon.className = `accordion`;
+                    let headDivEl = document.createElement(`div`);
+                    headDivEl.className = `head`;
+                    let span = document.createElement(`span`);
+                    span.textContent = line.title;
+                    headDivEl.appendChild(span);
+                    let moreBtn = document.createElement(`button`);
+                    moreBtn.textContent = `MORE`
+                    moreBtn.className = `button`;
+                    moreBtn.id = line._id;
+                    moreBtn.addEventListener('click', onClick);
+                    headDivEl.appendChild(moreBtn);
+                    // Extra 
+                    let extraDivEl = document.createElement(`div`);
+                    extraDivEl.className = `extra`;
+                    extraDivEl.style.display = `none`;
+                    let paraEl = document.createElement(`p`);
+                    let paraUrl = `http://localhost:3030/jsonstore/advanced/articles/details/${line._id}`;
+                    fetch(paraUrl)
+                        .then(response => response.json().then(data => paraEl.textContent = data.content).catch(err => console(`Error`)))
+                    .catch(err => console(`Error`))
+                    extraDivEl.appendChild(paraEl);
+                    acordeon.appendChild(headDivEl);
+                    acordeon.appendChild(extraDivEl);
+                    main.appendChild(acordeon);
+                }
+            }).catch(err => console(`Error`))
+        }).catch(err => console(`Error`))
+    function onClick(e) {
+        let acordeon = e.target.parentNode.parentNode;
+        if(e.target.textContent === `More`){
+            acordeon.children[1].style.display = `block`;
+            e.target.textContent = `Less`
+        }else {
+            acordeon.children[1].style.display = `none`;
+            e.target.textContent = `More`
+        }
+        // let currentId = e.target.id;
+    }
 }
+
+solution();
--- JS App/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
+++ JS App/02. JS-Applications-Asynchronous-Programming-Exercise-Resources/06.Blog/app.js
@@ -1,5 +1,55 @@
 function attachEvents() {
-    console.log('TODO...');
+    let viewBtn = document.querySelector('#btnViewPost');
+    let select = document.getElementById('posts');
+    let postUrl = `http://localhost:3030/jsonstore/blog/posts`;
+    let commentsUlEl = document.getElementById('post-comments');
+    let headOne = document.getElementById('post-title');
+    let postBody = document.getElementById('post-body');
+    fetch(postUrl)
+        .then(response => {
+            response.json()
+                .then(data => {
+                    for (const line in data) {
+                        // take data for dropdown manu
+                        let opiton = document.createElement('option');
+                        opiton.value = line
+                        opiton.textContent = data[line].title;
+                        select.appendChild(opiton);
+                    }
+                }
+                )
+        })
+    viewBtn.addEventListener(`click`, e => {
+        commentsUlEl.innerHTML = ``;
+        let currentPostId = select.value;
+        //create the current post 
+        fetch(`http://localhost:3030/jsonstore/blog/posts/${currentPostId}`)
+            .then(response => {
+                response.json()
+                    .then(data => {
+                        console.log(data.title);
+                        headOne.textContent = data.title
+                        console.log(data.body);
+                        postBody.textContent = data.body;
+                    })
+
+            })
+
+        //take comments 
+        fetch(`http://localhost:3030/jsonstore/blog/comments`)
+            .then(response => {
+                response.json()
+                    .then(data => {
+                        for (const key in data) {
+                            if (currentPostId === data[key].postId) {
+                                let currentComment = document.createElement(`li`);
+                                currentComment.textContent = data[key].text;
+                                commentsUlEl.appendChild(currentComment);
+                            }
+                        }
+                    })
+            })
+    })
 }
 
 attachEvents();
