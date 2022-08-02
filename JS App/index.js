e6b3a88efc25f8eb2a5cbb36d70dd464d9aa7586
Georgi <60046070+GeorgiDishkov@users.noreply.github.com>
2022-08-02 13:39:03 +0300

    Revert "Uploading"
    This reverts commit 549debcc1dadde30bc4dc067706c568855446101.

--- JS App/index.js
+++
@@ -1,8 +0,0 @@
-console.log(`Hasdfkasldk`);
-
-let div = document.getElementById('app');
-const list = document.getElementById("fragment");
-
-list.innerHTML = `<li><a href="#">Item</a></li>`;
-
-div.appendChild(list)
